d9e14be90607e6a2ca2eabda32c85259b42ecbc5
Francisco Chavez-Tejeda <francisco.chavez.509@outlook.com>
2021-04-26 21:26:00 -0700

    Progress check.

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+
+using WishList.Data;
+
 
 namespace WishList
 {
@@ -19,6 +23,7 @@ namespace WishList
         public void ConfigureServices(IServiceCollection services)
         {
 			services.AddMvc();
+			services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("WishList"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
